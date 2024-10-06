cd33e0647fc5011485583dc6670e5441ee0361c6
vivalavita <vitalina@MacBook-Air--vivalavita.local>
2024-10-06 04:30:50 +0300

    Split code into main script and modules

--- dna_rna_fastq_tools.py
+++ dna_rna_fastq_tools.py
@@ -1,10 +1,15 @@
-def run_dna_rna_tools(*args):
+from modules.dna_rna_module import transcribe, reverse, complement, reverse_complement
+from modules.fastq_module import count_gc, get_mean_quality
+
+
+def run_dna_rna_tools(*args: str) -> str:
+    """Runs DNA/RNA processing procedures on prescribed sequences"""
     procedure = args[-1]
     sequences = args[:-1]
 
     for seq in sequences:
         if not set(seq).issubset(set("ATCGUatcgu")) or ('T' in seq and 'U' in seq):
-            return f"Uncorrect sequence (not DNA or RNA): {seq}"
+            return f"Incorrect sequence (not DNA or RNA): {seq}"
 
     if procedure == "transcribe":
         fn = transcribe
@@ -24,56 +29,19 @@ def run_dna_rna_tools(*args):
     return result
 
 
-def transcribe(sequence):
-    return sequence.replace("T", "U").replace("t", "u")
-
-
-def reverse(sequence):
-    return sequence[::-1]
-
-
-def complement(sequence):
-    complement_dict = {
-        "A": "T",
-        "T": "A",
-        "C": "G",
-        "G": "C",
-        "U": "A",
-        "a": "t",
-        "t": "a",
-        "c": "g",
-        "g": "c",
-        "u": "a",
-    }
-    return "".join([complement_dict[nuc] for nuc in sequence])
-
-
-def reverse_complement(sequence):
-    return reverse(complement(sequence))
-
-
-def count_gc(sequence):
-    g_count = sequence.count('G') + sequence.count('g')
-    c_count = sequence.count('C') + sequence.count('c')
-    return (g_count + c_count) / len(sequence) * 100
-
-
-def get_mean_quality(quality):
-    return sum(ord(q) - 33 for q in quality) / len(quality)
-
-
-def filter_fastq(seqs, gc_bounds = (0, 100), length_bounds = (0, 2 ** 32), quality_threshold = 0):
+def filter_fastq(seqs: dict, gc_bounds: tuple = (0, 100), length_bounds: tuple = (0, 2 ** 32), quality_threshold: float = 0) -> dict:
+    """Filters FASTQ sequences based on GC bounds, length and quality threshold"""
     if isinstance(gc_bounds, (int, float)):
         gc_bounds = (0, gc_bounds)
-        
+
     gc_min, gc_max = gc_bounds
     length_min, length_max = length_bounds
 
     filtered_seqs = {}
-    
+
     for seq_id, (seq, quality) in seqs.items():
         gc_content = count_gc(seq)
-        if gc_min <= gc_content <= gc_max and length_min <= len(seq) <= length_max and get_mean_quality(quality) >= quality_threshold:
+        if (gc_min <= gc_content <= gc_max and length_min <= len(seq) <= length_max and get_mean_quality(quality) >= quality_threshold):
             filtered_seqs[seq_id] = (seq, quality)
-            
+
     return filtered_seqs
