d2ed1ec4080dcda1cb60a0013a8798b384914089
vivalavita <vitalina@MacBook-Air--vivalavita.local>
2024-10-13 10:19:24 +0300

    Improve code flake8 and black

--- bio_files_processor.py
+++ bio_files_processor.py
@@ -1,8 +1,7 @@
 def convert_multiline_fasta_to_oneline(input_fasta: str, output_fasta: str = None):
-
     """
     Converts a multiline FASTA file to oneline FASTA file
-    
+
     Args:
         input_fasta: path to the input FASTA file
         output_fasta: path to the output FASTA file (optional. If not provide, output file will have the same name as the input file with addition “_oneline.fasta”)
@@ -10,20 +9,20 @@ def convert_multiline_fasta_to_oneline(input_fasta: str, output_fasta: str = Non
     Return:
         None
     """
-    
+
     if output_fasta is None:
         output_fasta = f"{input_fasta.rsplit('.', 1)[0]}_oneline.fasta"
-    
-    with open(input_fasta, 'r') as infile, open(output_fasta, 'w') as outfile:
-        sequence = ''
-        header = ''
+
+    with open(input_fasta, "r") as infile, open(output_fasta, "w") as outfile:
+        sequence = ""
+        header = ""
         for line in infile:
             line = line.strip()
-            if line.startswith('>'):
+            if line.startswith(">"):
                 if sequence:
                     outfile.write(f"{header}\n{sequence}\n")
                 header = line
-                sequence = ''
+                sequence = ""
             else:
                 sequence += line
         if sequence:
@@ -36,15 +35,15 @@ def parse_blast_output(input_file: str, output_file: str):
 
     Args:
         input_file: path to the BLAST output file (txt format)
-        output_file: path to the output file where to save results 
+        output_file: path to the output file where to save results
 
     Returns:
         None
     """
-    with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
+    with open(input_file, "r") as infile, open(output_file, "w") as outfile:
         best_match = []
         in_significant_alignments = False
-        
+
         for line in infile:
             if "Sequences producing significant alignments:" in line:
                 in_significant_alignments = True
@@ -56,6 +55,6 @@ def parse_blast_output(input_file: str, output_file: str):
                         best_match.append(columns[0].strip())
 
         proteins = sorted(set(best_match))
-        
+
         for protein in proteins:
             outfile.write(f"{protein}\n")
